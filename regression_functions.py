efcbbbf7613409346ad63e3e0db84fba3a36ff21
moppitz <maxim.oppitz@epfl.ch>
2023-12-19 22:14:59 +0100

    Interactive graphs

--- regression_functions.py
+++ regression_functions.py
@@ -5,6 +5,7 @@ import statsmodels.api as sm
 import matplotlib.pyplot as plt
 import seaborn as sns
 from sklearn.preprocessing import MinMaxScaler
+import plotly.graph_objs as go
 
 def perform_regression(aggregated_genre_df, umbrella_genre, dep_var):
     """
@@ -106,3 +107,153 @@ def correlation_matrix(aggregated_genre_df, desired_dep_variables, indep_var, um
     sns.heatmap(correlation_matrix, annot=True)
     plt.title(f"Correlation matrix for {indep_var} and dependent variables for genre {umbrella_genre}")
     plt.show()
+
+def interactive_correlation_matrix(aggregated_genre_df, desired_dep_variables, indep_var, categories):
+
+    """
+    Creates an interactive Plotly heatmap displaying the correlation matrix for different categories.
+    
+    Parameters:
+    aggregated_genre_df (DataFrame): A DataFrame indexed by category (genre) with columns for the variables of interest.
+    desired_dep_variables (list of str): A list of strings representing the dependent variables to be included in the correlation matrix.
+    indep_var (str): The independent variable whose correlation with the dependent variables is to be analyzed.
+    categories (list of str): A list of categories (genres) for which the correlation matrix is to be created.
+
+    Returns:
+    plotly.graph_objs._figure.Figure: A Plotly Figure object containing the interactive heatmaps.
+
+    """
+
+    fig = go.Figure()
+
+    for umbrella_genre in categories:
+        genre_data = aggregated_genre_df.loc[umbrella_genre]
+        desired_columns = [indep_var] + desired_dep_variables
+        genre_data = genre_data[desired_columns]
+
+        genre_data.dropna(subset=desired_columns, inplace=True)
+        correlation_matrix = genre_data.corr()
+
+        heatmap = go.Heatmap(
+                z=correlation_matrix.values,
+                x=correlation_matrix.columns,
+                y=correlation_matrix.columns,
+                colorscale='magma',
+                visible=False,
+                name=umbrella_genre,
+                text=correlation_matrix.values.round(2),  # Display rounded correlation values
+                texttemplate="%{text}",  # Use texttemplate for displaying text values
+            )
+
+        fig.add_trace(heatmap)
+
+    # Update layout to add and move dropdown
+    fig.update_layout(
+        updatemenus=[
+            dict(
+                buttons=[
+                    dict(label=genre,
+                         method='update',
+                         args=[{'visible': [genre == g for g in categories]},
+                               {'title': f"Correlation Matrix for {genre}"}])
+                    for genre in categories
+                ],
+                direction="down",
+                showactive=True,
+                x=0.8,  # Adjust x position
+                y=1.25,  # Adjust y position
+                xanchor='center',
+                yanchor='top'
+            )
+        ],
+        title="Select a Genre to View its Correlation Matrix",
+    )
+
+    fig.data[0].visible = True
+
+    return fig
+
+def interactive_residuals_scatterplot(genre_data, dep_var, line=False):
+    """
+    Generates an interactive Plotly scatter plot of residuals for each feature in the dataset.
+
+    Parameters:
+    genre_data (pd.DataFrame): [Description]
+    dep_var (str): [Description]
+    line (bool): [Description, defaults to False]
+
+    Returns:
+    plotly.graph_objs._figure.Figure: [Description]
+    """
+    X_columns = genre_data.drop(columns=[dep_var]).columns
+    X = genre_data.drop(columns=[dep_var]).values
+    y = genre_data[dep_var].values
+
+    fig = go.Figure()
+
+    for i, feature in enumerate(X_columns):
+        partial_model = sm.OLS(X[:, i], np.delete(X, i, axis=1)).fit()
+        residual_X = X[:, i] - partial_model.predict(np.delete(X, i, axis=1))
+
+        partial_model_y = sm.OLS(y, np.delete(X, i, axis=1)).fit()
+        residual_y = y - partial_model_y.predict(np.delete(X, i, axis=1))
+
+        # Add the scatter plot for residuals
+        fig.add_trace(go.Scatter(
+            x=residual_X, 
+            y=residual_y, 
+            mode='markers', 
+            name=f'Feature {feature}',
+            visible=(i == 0)  # Only the first scatter plot is visible
+        ))
+
+        if line:
+            # Fit a regression line through the scatter plot
+            line_params = np.polyfit(residual_X, residual_y, 1)
+            line_x = np.linspace(min(residual_X), max(residual_X), 100)
+            line_y = np.polyval(line_params, line_x)
+
+            # Add the regression line
+            fig.add_trace(go.Scatter(
+                x=line_x, 
+                y=line_y, 
+                mode='lines', 
+                name='Regression Line',
+                visible=(i == 0),  # Only the first regression line is visible
+                line=dict(color='red')
+            ))
+
+    # Create dropdown buttons for feature selection
+    buttons = []
+    for i, feature in enumerate(X_columns):
+        visibility = [False] * (len(X_columns) * (2 if line else 1))  # Initialize all to false
+        visibility[i * (2 if line else 1)] = True  # Toggle scatter plot
+        if line:
+            visibility[i * 2 + 1] = True  # Toggle regression line
+
+        button = dict(
+            label=f'Feature {feature}',
+            method='update',
+            args=[{'visible': visibility},
+                  {'title': f'Effect of Feature {feature}'}]
+        )
+        buttons.append(button)
+
+    # Update layout to add dropdown
+    fig.update_layout(
+        updatemenus=[{
+            'buttons': buttons,
+            'direction': 'down',
+            'showactive': True,
+            'x': 1,  # x = 0.5 positions the button in the center of the graph horizontally
+            'y': 1.2,  # y > 1 positions the button above the top of the graph
+            'xanchor': 'center',  # 'center' ensures that the middle of the button aligns with x position
+            'yanchor': 'top'  # 'top' ensures the button aligns above the graph based on the y position
+
+        }],
+        title=f"Effect of Feature on {dep_var}",
+        xaxis_title="Feature (residualized)",
+        yaxis_title=f"{dep_var} (residualized)"
+    )
+
+    return fig
